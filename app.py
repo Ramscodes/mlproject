483b68035c94cf22588d42e83a28dbf12861f538
Ramachari <ramanumarla18113@gmail.com>
2022-10-28 02:43:32 +0530

    updated the app

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "lets start building the world with machine learning..."
+    return "lets start living life without worries # NO_REGRETS , # NO_WORRIES..."
 
 if __name__=="__main__":
     app.run(debug=True)
